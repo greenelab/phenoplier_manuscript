09c1026ee89be65db5fe681a71bcb1fcd1bf1072
Milton Pividori <miltondp@gmail.com>
2023-06-13 14:33:23 -0400

    build_latex.sh: split main text and supplementary information

--- output/latex/build_latex.sh
+++ output/latex/build_latex.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+set -e
 
 latexmk -shell-escape
+
+# split
+pdftk manuscript.pdf cat 1-26 output main_text.pdf
+pdftk manuscript.pdf cat 27-end output supplementary_information.pdf
+
+rm manuscript.pdf
+
