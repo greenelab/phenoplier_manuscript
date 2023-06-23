bd69eacc74e6333cc2701731734438931d313716
Milton Pividori <miltondp@gmail.com>
2023-06-23 16:00:09 -0400

    build_latex.sh: change output file names

--- output/latex/build_latex.sh
+++ output/latex/build_latex.sh
@@ -4,8 +4,8 @@ set -e
 latexmk -shell-escape
 
 # split
-pdftk manuscript.pdf cat 1-23 output main_text.pdf
-pdftk manuscript.pdf cat 24-end output supplementary_information.pdf
+pdftk manuscript.pdf cat 1-23 output manuscript-main_text.pdf
+pdftk manuscript.pdf cat 24-end output manuscript-supplementary_information.pdf
 
 #rm manuscript.pdf
 
