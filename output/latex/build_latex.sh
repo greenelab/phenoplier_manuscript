b70312be592d54781383a69a230ab922ee91bff0
Milton Pividori <miltondp@gmail.com>
2023-08-03 12:36:49 -0600

    add script to replace references to supplementary material by their number

--- output/latex/build_latex.sh
+++ output/latex/build_latex.sh
@@ -14,6 +14,10 @@ csplit manuscript.tex /%SUPPLEMENTARY_INFORMATION_STARTS_HERE%/ '{*}'
 mv xx00 manuscript-main_text.tex
 echo "\end{document}" >> manuscript-main_text.tex
 
-# the supplementary tex file is not needed (cannot be compiled at this point anyway)
-# mv xx01 manuscript-supplementary_information.tex
-rm xx01
+# the supplementary tex file is not compilable, but it is useful to extract a list
+# of labels for the supplementary figures and tables
+mv xx01 manuscript-supplementary_information.tex
+# rm xx01
+
+# replace in main text file references to supplementary information
+python3 replace_labels.py manuscript-supplementary_information.tex manuscript-main_text.tex
